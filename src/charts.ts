d463811b0757e73cd6448ef6539b8f84a24848d1
Leonard <leonard@mimisiku.dev>
2022-08-29 15:26:30 +0200

    fixes

--- src/charts.ts
+++ src/charts.ts
@@ -1,17 +1,25 @@
 import { Chart, Util } from '@antv/g2';
 
-export function renderRepartitionChart(container: HTMLElement) {
-    const data = [
-        { type: 'Real estate', value: 70 },
-        { type: 'Crypto', value: 20 },
-        { type: 'Life insurance', value: 10 }
-    ];
+export function renderRepartitionChart(container: HTMLElement, data: {
+    chart: {
+        id:string
+        name: {en: string, fr: string}
+        percent:number;
+        price: number
+    }[];
+}) {
+    const parsed = data.chart.map(item => ({
+        id: item.id,
+        name: item.name.en,
+        percent: item.percent,
+        price: item.price,
+    }));
     const chart = new Chart({
         container,
         autoFit: true,
         height: 500,
     });
-    chart.data(data);
+    chart.data(parsed);
     
     chart.coordinate('theta', {
         radius: 0.75
@@ -23,35 +31,34 @@ export function renderRepartitionChart(container: HTMLElement) {
     chart
     .interval()
     .adjust('stack')
-    .position('value')
-    .color('type', ['#063d8a', '#1770d6', '#47abfc', '#38c060'])
+    .position('percent')
+    .color('name', ['#063d8a', '#1770d6', '#47abfc', '#38c060'])
     .style({ opacity: 0.4 })
     .state({
         active: {
         style: (element) => {
             const shape = element.shape;
             return {
-            matrix: Util.zoom(shape, 1.1),
+                matrix: Util.zoom(shape, 1.1),
             };
         }
         }
-    })
-    .label('type', (val) => {
-        const opacity = val === 'Crypto' ? 1 : 0.5;
-        return {
-        offset: -30,
-        style: {
-            opacity,
-            fill: 'white',
-            fontSize: 12,
-            shadowBlur: 2,
-            shadowColor: 'rgba(0, 0, 0, .45)',
-        },
-        content: (obj) => {
-            return obj.type + '\n' + obj.value + '%';
-        },
-        };
     });
+    /*.label('name', (_val) => {
+        return {
+            offset: -30,
+            style: {
+                opacity: 1,
+                fill: 'white',
+                fontSize: 12,
+                shadowBlur: 2,
+                shadowColor: 'rgba(0, 0, 0, .45)',
+            },
+            content: (obj) => {
+                return obj.name + '\n' + obj.percent + '%';
+            },
+        };
+    });*/
       
     chart.interaction('element-single-selected');
     chart.render();
