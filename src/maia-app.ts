a7577369ea942004939a3a8bede95dd6f411a5c4
Leonard <leonard@mimisiku.dev>
2022-08-29 04:57:10 +0200

    use icon

--- src/maia-app.ts
+++ src/maia-app.ts
@@ -132,7 +132,8 @@ export class MaiaApp extends Root {
 							<polyline points="16 17 21 12 16 7"/><line x1="21" y1="12" x2="9" y2="12"/>
 						</svg>
 					</button>
-					` : html`Connect`}
+					` : html`
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-in"><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"/><polyline points="10 17 15 12 10 7"/><line x1="15" y1="12" x2="3" y2="12"/></svg>`}
 					</button>
 				</div>
 				<button class="messages-btn">
