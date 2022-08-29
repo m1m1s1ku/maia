ab88fb899fbf267644b5e4a3a2b8aeec6c88ddfa
Leonard <leonard@mimisiku.dev>
2022-08-29 05:11:50 +0200

    x

--- src/maia-app.ts
+++ src/maia-app.ts
@@ -95,9 +95,9 @@ export class MaiaApp extends Root {
 					<div class="search-wrapper">
 						<input class="search-input" type="text" placeholder="Search.">
 						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-search" viewBox="0 0 24 24">
-						<defs></defs>
-						<circle cx="11" cy="11" r="8"></circle>
-						<path d="M21 21l-4.35-4.35"></path>
+							<defs></defs>
+							<circle cx="11" cy="11" r="8"></circle>
+							<path d="M21 21l-4.35-4.35"></path>
 						</svg>
 					</div>
 				</div>
