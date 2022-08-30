d6b657a2794ae60f0d1fc447e2629c973af94ca6
Leonard <leonard@mimisiku.dev>
2022-08-30 02:48:55 +0200

    remove deprecated code

--- src/core/strategies/Page.ts
+++ src/core/strategies/Page.ts
@@ -1,22 +1,5 @@
 import { LitElement } from 'lit';
 
-export interface PageHelmet {
-    title: string | null;
-    description: string | null;
-    type: string | null;
-    image: string | null;
-    slug: string | null;
-}
 export default class Page extends LitElement {
-    public get head(): PageHelmet {
-        return {
-            title: null,
-            description: null,
-            type: null,
-            image: null,
-            slug: null
-        };
-    }
-
     createRenderRoot(): this { return this; }
 }
